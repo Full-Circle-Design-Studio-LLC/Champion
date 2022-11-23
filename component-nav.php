9367d5369c3f81711b5dcdd1f11c937eb41350ba
westonh2 <weston@full-circle.design>
2022-11-23 14:40:48 -0500

    .

--- component-nav.php
+++ component-nav.php
@@ -1,3 +1,11 @@
+<header class="col-12 p-0 float-left">
+<img src="<?php echo get_template_directory_uri(); ?>/assets//banner/left.png" alt="left-image" class="left-image">
+<div class="logo-container">
+<a href="<?php echo get_site_url(); ?>/home"><img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="Champion Trophies & Awards Logo"></a>
+</div>
+<img src="<?php echo get_template_directory_uri(); ?>/assets//banner/right.png" alt="right-image" class="right-image">
+</header>
+
 <nav class="navbar navbar-expand-lg navbar-dark col-12 p-0 float-left">
     <h4 class="d-lg-none col-4 offset-4">Home</h4>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
